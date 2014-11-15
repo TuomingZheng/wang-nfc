b8c4ee1be32c72dbf2c4b670728526d04983eb41
TuomingZheng <zhengtuoming@yeah.net>
2014-11-15 20:10:22 +0800

    提交数据库类

--- PostCard/src/com/yigao/nfc/postcard/database/PostCardSQLiteOpenHelper.java
+++ PostCard/src/com/yigao/nfc/postcard/database/PostCardSQLiteOpenHelper.java
@@ -1,18 +1,24 @@
 
 package com.yigao.nfc.postcard.database;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class PostCardSQLiteOpenHelper extends SQLiteOpenHelper {
 
+    public PostCardSQLiteOpenHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
-    public void onCreate(SQLiteDatabase arg0) {
+    public void onCreate(SQLiteDatabase db) {
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 
